9fda9456a4b4a313f42895cf2bf5c878a59cd56f
Habtamu Gebre <habtamuwolde92@gmail.com>
2022-08-10 14:53:42 +0530

    rock paper scissor game

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 function getComputerChoice(){
-    const pick = ['rock', 'paper', 'scissors'];
+    const pick = ['rock', 'paper', 'scissor'];
     return pick[Math.floor(Math.random() * pick.length)];
 
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "rock" &&  computerSelection === "paper"){
+        return "You Lose! Paper beats rock"
+    }
+    else if(playerSelection==="rock" && computerSelection==="scissor"){
+        return "You win! rock breaks scissor"
+    }
+    else if(playerSelection==="paper" && computerSelection==="rock"){
+        return "You win! paper covers rock"
+    }
+    else if(playerSelection==="paper" && computerSelection==="scissor"){
+        return "You Lose! scissor cuts paper"
+    }
+    else if(playerSelection === "scissor" && computerSelection === "rock"){
+        return "You Lose! Rock breaks scissor"
+    }
+    else if(playerSelection==="scissor" && computerSelection==="paper"){
+        return "You win! scissor cuts paper"
+    }
+    else if(playerSelection === computerSelection){
+        return "draw"
+    }
+    else{
+        return "invalid input"
+    }
+  }
+   
+
+
+function game(){
+    
+    
+    for(let i= 0; i<5; i++){
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("Your choice").toLowerCase();
+        console.log(playRound(playerSelection, computerSelection))
+    }
+}
+game()
